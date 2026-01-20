0e5bc1c3472c59c90cf759d9686bda003531671e
lwidajew <lwidajew@umich.edu>
2026-01-20 14:55:30 -0500

    Updated Label and Comments
    Label in main
    Comment in moveableListbox

--- moveableListbox.py
+++ moveableListbox.py
@@ -1,4 +1,5 @@
 class moveItems:
+    # use this? -- https://stackoverflow.com/questions/52763415/how-to-move-items-up-or-down-a-listbox-in-tkinter-without-losing-their-selection
     def __init__(self):
         pass
         
